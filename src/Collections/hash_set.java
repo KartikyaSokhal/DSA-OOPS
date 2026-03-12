9a64d23ecbcf910fa8e89dc9b1e8a6dd70e40322
Kartikya Sokhal <kartikyasokhal28@gmail.com>
2026-03-12 12:47:21 +0530

    hashset basics

--- src/Collections/hash_set.java
+++ src/Collections/hash_set.java
@@ -1,4 +1,52 @@
 package Collections;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class hash_set {
+    public static void main(String[] args){
+        HashSet<String> fruits = new HashSet<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        fruits.add("DragonFruit");
+        fruits.add("PineApple");
+
+
+        // size of hashset
+        int size = fruits.size();
+
+        System.out.println(size);
+
+        // ==== ITERATION
+
+//        System.out.println("\n--- Iterating using for-each ----");
+//        for (String fruit : fruits) {
+//            System.out.println("Fruit: " + fruit);
+//        }
+//
+//        System.out.println("\n--- iterating using iterator --");
+//        Iterator<String> interator = fruits.iterator();
+//        while(interator.hasNext()){
+//            System.out.println("Fruit: " + interator.next());
+//        }
+
+        fruits.remove("Apple");
+
+        System.out.println("\n--- Iterating using for-each ----");
+        for (String fruit : fruits) {
+            System.out.println("Fruit: " + fruit);
+        }
+
+        fruits.removeAll(fruits);
+
+        System.out.println("\n--- after removeAll ----");
+        for (String fruit : fruits) {
+            System.out.println("Fruit: " + fruit);
+        }
+
+    }
+
+
 }
