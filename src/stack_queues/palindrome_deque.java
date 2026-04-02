1b61d438bccc0021aef62138a70878377f27ff7d
Kartikya Sokhal <kartikyasokhal28@gmail.com>
2026-04-02 11:42:09 +0530

    checking palindrome or not using deque

--- src/stack_queues/palindrome_deque.java
+++ src/stack_queues/palindrome_deque.java
@@ -1,4 +1,26 @@
 package stack_queues;
-
+import java.util.*;
 public class palindrome_deque {
+    public static void main(String[] args){
+        String s = "m";
+        Deque <Character> dq = new ArrayDeque<>();
+        for (int i=0;i<s.length();i++){
+            dq.addLast(s.charAt(i));
+        }
+        while(!dq.isEmpty()){
+            if (dq.peekFirst()!= dq.peekLast()){
+                System.out.println("not Palindrome");
+                break;
+            } else if (dq.size()==1){
+                break;
+            } else {
+                dq.removeLast();
+                dq.removeFirst();
+            }
+        }
+
+        if (dq.isEmpty() || dq.size() ==1){
+            System.out.println("palindrome");
+        }
+    }
 }
