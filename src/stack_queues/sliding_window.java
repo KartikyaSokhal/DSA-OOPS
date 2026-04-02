68d72adf44a7a89d91e21c96a3ed241097500dd7
Kartikya Sokhal <kartikyasokhal28@gmail.com>
2026-04-02 11:43:05 +0530

    sliding window (finding max)

--- src/stack_queues/sliding_window.java
+++ src/stack_queues/sliding_window.java
@@ -1,4 +1,29 @@
 package stack_queues;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class sliding_window {
+    public static void main(String[] args){
+        int arr[] = {10,-3,2,40,20,30,10,5};
+        int k = 3;
+
+        Deque <Integer> dq = new ArrayDeque<>();
+
+        for (int i=0;i<arr.length;i++){
+            while(!dq.isEmpty() && dq.peekFirst()==i-k){
+                dq.removeFirst();
+            }
+
+            while(!dq.isEmpty() && arr[dq.peekLast()] < arr[i]){
+                dq.removeLast();
+            }
+
+            dq.add(i);
+
+            if (i>=k-1){
+                System.out.println(arr[dq.peekFirst()]);
+            }
+        }
+    }
 }
